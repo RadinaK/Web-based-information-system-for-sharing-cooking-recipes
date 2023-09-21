0e6a6a3f21750ae08f61f6616b3900e058105c05
Radina Krasteva <radinakrsteva@gmail.com>
2023-09-21 14:03:52 +0300

    start

---
+++ src/test/java/bg/tuplovdiv/cookingrecipes/CookingRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.tuplovdiv.cookingrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
